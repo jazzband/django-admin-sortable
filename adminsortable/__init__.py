cffbd8534e7f0a4e1b2e3d17857055a781f149e0
Brandon Taylor <brandon@iambrandontaylor.com>
2013-02-28 22:07:16 -0500

    Working on updating documentation and sample project

--- adminsortable/__init__.py
+++ adminsortable/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (1, 3, 9)  # following PEP 386
+VERSION = (1, 4, 0)  # following PEP 386
 DEV_N = None
 
 
--- adminsortable/fields.py
+++ adminsortable/fields.py
@@ -4,14 +4,16 @@ from django.db.models.fields.related import ForeignKey
 class SortableForeignKey(ForeignKey):
     """
     Field simply acts as a flag to determine the class to sort by.
-    This field replaces previous functionality where `sortable_by` was definied as a model property
-    that specified another model class.
+    This field replaces previous functionality where `sortable_by` was
+    definied as a model property that specified another model class.
     """
 
     def south_field_triple(self):
         try:
             from south.modelsinspector import introspector
-            cls_name = '{0}.{1}'.format(self.__class__.__module__, self.__class__.__name__)
+            cls_name = '{0}.{1}'.format(
+                self.__class__.__module__,
+                self.__class__.__name__)
             args, kwargs = introspector(self)
             return cls_name, args, kwargs
         except ImportError:
