4c2925e1f0fb9559ff8fa070d4e4896ff2f5d9e5
Brandon Taylor <alsoicode@gmail.com>
2014-09-12 08:51:36 -0400

    Added SOUTH_TESTS_MIGRATE to settings.

--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -22,6 +22,8 @@ DATABASES = {
     }
 }
 
+SOUTH_TESTS_MIGRATE = False
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
