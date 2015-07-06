3fd7d4aff3606cdf94651383f92bcda58faa4338
Brandon Taylor <alsoicode@gmail.com>
2015-07-05 23:03:24 -0400

    Re-organized setup.

--- setup.py
+++ setup.py
@@ -6,17 +6,8 @@ except:
     README = None
 
 setup(
-    name='django-admin-sortable',
-    version=__import__('adminsortable').__version__,
-    description='Drag and drop sorting for models and inline models in Django admin.',
-    long_description=README,
-    license='APL',
     author='Brandon Taylor',
     author_email='alsoicode@gmail.com',
-    url='https://github.com/iambrandontaylor/django-admin-sortable',
-    packages=find_packages(exclude=['sample_project']),
-    zip_safe=False,
-    include_package_data=True,
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Environment :: Web Environment',
                  'Framework :: Django',
@@ -25,4 +16,14 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python',
                  'Topic :: Utilities'],
+    description='Drag and drop sorting for models and inline models in Django admin.',
+    include_package_data=True,
+    install_requires=['django'],
+    license='APL',
+    long_description=README,
+    name='django-admin-sortable',
+    packages=find_packages(exclude=['sample_project']),
+    url='https://github.com/iambrandontaylor/django-admin-sortable',
+    version=__import__('adminsortable').__version__,
+    zip_safe=False
 )
