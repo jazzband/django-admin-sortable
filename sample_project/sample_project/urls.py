684864be333fdb37473ec48794cad7b17a1393f3
Venelin Stoykov <venelin@magicsolutions.bg>
2016-06-24 14:06:54 +0300

    Update sample project for latest django versions

--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'test_project.views.home', name='home'),
     # url(r'^test_project/', include('test_project.foo.urls')),
@@ -15,4 +15,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+]
