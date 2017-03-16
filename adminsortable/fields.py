b0df1ff70123dd80138deb4bcbe1a7ba2647b87e
Jaap Roes <jroes@leukeleu.nl>
2017-03-16 15:19:26 +0100

    Remove south_field_triple method as South does not support Django >= 1.7

--- adminsortable/fields.py
+++ adminsortable/fields.py
@@ -7,14 +7,4 @@ class SortableForeignKey(ForeignKey):
     This field replaces previous functionality where `sortable_by` was
     defined as a model property that specified another model class.
     """
-
-    def south_field_triple(self):
-        try:
-            from south.modelsinspector import introspector
-            cls_name = '{0}.{1}'.format(
-                self.__class__.__module__,
-                self.__class__.__name__)
-            args, kwargs = introspector(self)
-            return cls_name, args, kwargs
-        except ImportError:
-            pass
+    pass
