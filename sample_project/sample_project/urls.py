de6a768cebf1af523e75544fdf2abadc1586c9cb
Jaap Roes <jroes@leukeleu.nl>
2017-03-16 16:07:13 +0100

    No need to use include for admin.site.urls (it’s deprecated)

--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
