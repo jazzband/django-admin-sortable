356d88dfde42b1da67297213e1250ef47dbb4690
Jaap Roes <jroes@leukeleu.nl>
2017-03-16 15:09:17 +0100

    Remove ordering field lookup fallback

--- adminsortable/admin.py
+++ adminsortable/admin.py
@@ -184,9 +184,6 @@ class SortableAdmin(SortableAdminBase, ModelAdmin):
             try:
                 order_field_name = opts.model._meta.ordering[0]
             except (AttributeError, IndexError):
-                # for Django 1.5.x
-                order_field_name = opts.ordering[0]
-            except (AttributeError, IndexError):
                 order_field_name = 'order'
 
             objects = objects.order_by(sortable_by_expression, order_field_name)
